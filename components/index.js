aafeaa9f80c25eb591f29b01986368dc18e000ef
anuragsharma1111994 <anuragsharma1111994@gmail.com>
2022-01-01 23:09:32 +0530

    002 - Post Done

--- components/index.js
+++ components/index.js
@@ -2,4 +2,5 @@ export { default as PostCard } from "./PostCard";
 export { default as Categories } from "./Categories";
 export { default as PostWidgit } from "./PostWidgit";
 export { default as Headers } from "./Headers";
+export { default as Layout } from "./Layout";
 
